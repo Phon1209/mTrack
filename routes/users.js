0681f653559abc5d6825beeb99d4d791415952e2
Phon1209 <ptphon@gmail.com>
2022-03-09 23:01:01 -0500

    Add route for registering user

---
+++ routes/users.js
@@ -0,0 +1,84 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const { check, validationResult } = require("express-validator");
+
+// User Schema
+const User = require("../models/User");
+
+// @route   POST  api/users
+// @desc    Register a normal user
+// @access  Public
+router.post(
+  "/",
+  [
+    check("name", "name is required").not().isEmpty(),
+    check("email", "valid email is required").isEmail(),
+    check("password", "Please include password").not().isEmpty(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    // Some error occurs when validate data
+    if (!errors.isEmpty()) {
+      console.error("Some Error occured while validating data");
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    // Extract data from the body of the request (processed by middleware)
+    const { name, email, password } = req.body;
+
+    try {
+      // Check if user already existed with email
+      let user = await User.findOne({ email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: `User with email: ${email} already existed` });
+      }
+
+      // Create new schema object
+      user = new User({
+        name,
+        email,
+        password,
+      });
+
+      // Hashing the password
+      const salt = await bcrypt.genSalt();
+      user.password = await bcrypt.hash(password, salt);
+
+      // Save to the DB
+      await user.save();
+
+      // return res.status(201).json({ msg: "Register Successful" });
+
+      // jwt token generate
+      // payload = what we want to send back to user
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtPrivateKey"),
+        {
+          expiresIn: 3600,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json(token);
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).json({ errors: "Server has some problem" });
+    }
+  }
+);
+
+module.exports = router;
