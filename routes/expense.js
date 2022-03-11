599358b556c1e3806a2b16ba17f9187fb31cd26f
Phon1209 <ptphon@gmail.com>
2022-03-11 18:39:14 -0500

    Add Expense endpoint
    - api/tracker/expense/     for get all entries/ craete new entry
    - api/tracker/expense/:id  for get specific entry

---
+++ routes/expense.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const auth = require("../middleware/auth");
+const router = express.Router();
+
+// Expense Schema
+const Expense = require("../models/Expense");
+
+// @route   GET   api/tracker/expense
+// @desc    get all expense data
+// @access  Private
+router.get("/", auth, async (req, res) => {
+  const param = {};
+  if (req.query.before || req.query.after) param.date = {};
+  if (req.query.after) param.date["$gte"] = new Date(req.query.after);
+  if (req.query.before) param.date["$lt"] = new Date(req.query.before);
+
+  try {
+    const expense = await Expense.find(param);
+
+    res.json(expense);
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
+});
+
+// @route   POST   api/tracker/expense
+// @desc    add expense data
+// @access  Private
+router.post("/", auth, async (req, res) => {
+  try {
+    const { amount, desc, date } = req.body;
+
+    const expense = new Expense({
+      amount,
+      date,
+      desc,
+    });
+    await expense.save();
+
+    return res.json(expense);
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
+});
+
+// @route   GET   api/tracker/expense/:id
+// @desc    get one expense entry
+// @access  Private
+router.get("/:id", auth, async (req, res) => {
+  try {
+    const expense = await Expense.findById(req.params.id);
+
+    if (!expense) {
+      return res.status(404).json({ error: "Entries not found" });
+    }
+
+    return res.json(expense);
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
+});
+
+// @TODO: end point for pagination
+// @route   GET   api/tracker/expense/page/:id
+// @desc    get n entries (default: 10) for page
+// @access  Private
+module.exports = router;
